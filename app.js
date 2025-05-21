6491b7b53fa291a5d824cf357224311c6a37cac4
ecavalcanti683 <ecavalcanti.dg@outlook.com>
2025-05-21 12:11:43 +0200

    Status

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
+// https://www.npmjs.com/package/dotenv t
 require("dotenv").config();
 
 // ℹ️ Connects to the database
